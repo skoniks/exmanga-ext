85cfaba95707f5c464922fab78720ac999b7458e
skoniks <skolya16@ya.ru>
2022-06-20 20:01:56 +0300

    VPN support update

--- scripts/content.js
+++ scripts/content.js
@@ -1,35 +1,52 @@
 console.log('ExManga loaded');
 
 var preload = `
-window.fetch = (...request) =>
-  new Promise((resolve) => {
+window.fetch = (...request) => {
+  const sendMessage = (resolve, ...request) => {
     chrome.runtime.sendMessage(
       '${chrome.runtime.id}',
       { request, location },
       ({ body, type, init, message }) => {
-        if (message) {
-          const element = document.createElement('span');
-          element.innerText = message;
-          let container = document.querySelector('.notify-container');
-          if (!container) {
-            container = document.createElement('div');
-            container.className = 'notify-container';
-            document.body.appendChild(container);
-          }
-          container.appendChild(element);
-          setTimeout(() => {
-            element.className = 'show';
-          }, 100);
-          setTimeout(() => {
-            element.className = '';
-          }, 7300);
-          setTimeout(() => element.remove(), 7500);
-        }
+        if (message) drawNotify(message);
         const blob = new Blob([body], { type });
         resolve(new Response(blob, init));
       },
     );
+  };
+  const drawNotify = (message) => {
+    const element = document.createElement('span');
+    element.innerText = message;
+    let container = document.querySelector('.notify-container');
+    if (!container) {
+      container = document.createElement('div');
+      container.className = 'notify-container';
+      document.body.appendChild(container);
+    }
+    container.appendChild(element);
+    setTimeout(() => {
+      element.className = 'show';
+    }, 100);
+    setTimeout(() => {
+      element.className = '';
+    }, 7300);
+    setTimeout(() => element.remove(), 7500);
+  };
+  return new Promise((resolve) => {
+    if (typeof request[0] === 'object') {
+      let { url, method, headers } = request[0];
+      headers = Object.fromEntries(headers.entries());
+      if (request[0].bodyUsed) {
+        request[0].json().then((body) => {
+          sendMessage(resolve, url, { method, headers, body });
+        });
+      } else {
+        sendMessage(resolve, url, { method, headers });
+      }
+    } else {
+      sendMessage(resolve, ...request);
+    }
   });
+};
 `;
 
 document.documentElement.setAttribute('onreset', preload);
